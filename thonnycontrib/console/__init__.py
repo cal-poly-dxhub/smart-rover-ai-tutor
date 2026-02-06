5da7d1872f6fbdcdc87adf36696556ddb4360a4d
Nathan St. John <natestjohn05@gmail.com>
2026-02-05 19:50:01 -0800

    Code duplication fix

--- thonnycontrib/console/__init__.py
+++ thonnycontrib/console/__init__.py
@@ -11,15 +11,3 @@ __all__ = [
     "CommandHistory",
     "BuiltinCommandHandler"
 ]
-
-from thonnycontrib.console.terminal_controller import TerminalController
-from thonnycontrib.console.command_executor import CommandExecutor
-from thonnycontrib.console.command_history import CommandHistory
-from thonnycontrib.console.builtin_commands import BuiltinCommandHandler
-
-__all__ = [
-    "TerminalController",
-    "CommandExecutor",
-    "CommandHistory",
-    "BuiltinCommandHandler"
-]
